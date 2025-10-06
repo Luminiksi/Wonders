e0d23e57e2fd8a26a8509aef2cbb4bb88ba551c0
luminiksi <luminiksi@ya.ru>
2025-10-06 10:40:10 +0300

    add: functional for FAQ blog

---
+++ constants.js
@@ -0,0 +1,47 @@
+export const arrayFAQS = [
+    {
+        id: 0,
+        title: "Alpine Bus Journeys?",
+        text: "Forget crowded cities! Alpine Bus Journeys are your comfy seat with\n" +
+            "panoramic windows to breathtaking mountain vistas, charming villages, and air so fresh\n" +
+            "it might make you yodel (please don't, unless you're outside). It's transportation with\n" +
+            "a side of unparalleled scenery (and maybe a hairpin turn)."
+    },
+    {
+        id: 1,
+        title: "What about seating options?",
+        text: "Generally, you'll find comfy standard seats (great views, great value,\n" +
+            "maybe great new friends) and perhaps slightly better seats with extra legroom. All offer\n" +
+            "a front-row seat to the greatest show on Earth – the Alps!"
+    },
+    {
+        id: 2,
+        title: "What is the Alpine Wallet?",
+        text: "It's your digital stash of cash that makes booking bus tickets smoother\n" +
+            "than a perfectly paved mountain road. Load it up and watch your travel dreams become\n" +
+            " reality faster than a bus goes downhill."
+    },
+    {
+        id: 3,
+        title: "Why a Bus Station Fee?",
+        text: "A small contribution to ensure the station is clean, has reasonably\n" +
+            "fresh mountain air (filtered from bus fumes), and enough signs pointing you towards\n" +
+            "your bus, not away from it. Sometimes covers the cost of moving rogue bicycles."
+    },
+    {
+        id: 4,
+        title: "Why are buses late?",
+        text: "Sometimes it's the weather (snow happens!), sometimes a sudden flock of\n" +
+            "particularly slow-moving sheep on the road, and sometimes the driver just had to stop for\n" +
+            "a photo of that view. Or maybe they got stuck behind a tractor. Welcome to\n" +
+            "\"Mountain Road Time\"!"
+    },
+    {
+        id: 5,
+        title: "Peak Season Surcharge?",
+        text: "It's the little extra you pay to enjoy the authentic experience of\n" +
+            "sharing breathtaking views with a lot of your closest friends (who you haven't met yet)\n" +
+            "while strategically placing your bag in the overhead compartment. Guarantees a vibrant,\n" +
+            " energetic journey!"
+    }
+]
---
+++ script.js
@@ -0,0 +1,53 @@
+import * as Constants from './constants.js'; // Импорт всех констант в один объект
+
+const faqsItems = document.getElementById("faqs-items");
+let activeFAQsElement = '0'
+
+const fillFAQsItems = (FAQS) => {
+    faqsItems.innerHTML = '<h2>FAQs.</h2>'
+    FAQS.forEach((item) => {
+        faqsItems.insertAdjacentHTML('beforeend', `
+            <div class="faqs-item">
+                <div class="faqs-item-title">
+                    <h5 class="faq-title">${item.title}</h5>
+                    <button class="faqs-btn" id="faqs-btn-${item.id}">
+                        <img src="./assets/icons/openElement.svg" alt="closeElement">                    
+                    </button>
+                </div>
+                <div class="faqs-item-text hidden" id="faqs-item-text-${item.id}">
+                    <p class="faq-text">${item.text}</p>
+                </div>
+            </div>
+        `);
+    })
+
+    const firstBtn = document.getElementById("faqs-btn-0")
+    firstBtn.innerHTML = '<img src="./assets/icons/closeElement.svg" alt="closeElement">'
+    const firsText = document.getElementById("faqs-item-text-0")
+    firsText.classList.toggle('hidden');
+}
+
+fillFAQsItems(Constants.arrayFAQS)
+
+
+faqsItems.addEventListener('click', function (event) {
+    const target = event.target;
+    if (target.tagName === 'IMG') {
+        const activeBtn = document.getElementById(`faqs-btn-${activeFAQsElement}`)
+        const activeText = document.getElementById(`faqs-item-text-${activeFAQsElement}`)
+        activeBtn.innerHTML = '<img src="./assets/icons/openElement.svg" alt="openElement">'
+        activeText.classList.toggle('hidden');
+
+        const parentBtn = target.parentElement;
+        const id = parentBtn.id.substring(9)
+        const textElement = document.getElementById(`faqs-item-text-${id}`)
+
+        if (textElement.classList.contains('hidden')) {
+            parentBtn.innerHTML = '<img src="./assets/icons/closeElement.svg" alt="closeElement">'
+        } else {
+            parentBtn.innerHTML = '<img src="./assets/icons/openElement.svg" alt="openElement">'
+        }
+        textElement.classList.toggle('hidden');
+        activeFAQsElement = id
+    }
+});
