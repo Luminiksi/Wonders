664e575fc1b8a4d348151420bd9123f74d5b6793
luminiksi <luminiksi@ya.ru>
2025-10-10 13:38:51 +0300

    add: calendar - create function renderBetweenDates

--- calendar.js
+++ calendar.js
@@ -50,11 +50,19 @@ export class Calendar {
         })
 
         this.#leftArea.addEventListener('click', (event) => {
-            this.#selectDay(event.target, this.#currentDate, 5)
+            let isChange = this.#selectDay(event.target, this.#currentDate, 5)
+            if (isChange) {
+                this.#renderBetweenDates(this.#currentDate, 'left-')
+                this.#renderBetweenDates(this.#nextDate, 'right-')
+            }
         })
 
         this.#rightArea.addEventListener('click', (event) => {
-            this.#selectDay(event.target, this.#nextDate, 6)
+            let isChange = this.#selectDay(event.target, this.#nextDate, 6)
+            if (isChange) {
+                this.#renderBetweenDates(this.#currentDate, 'left-')
+                this.#renderBetweenDates(this.#nextDate, 'right-')
+            }
         })
     }
 
@@ -66,10 +74,21 @@ export class Calendar {
         const id = target.getAttribute('id')
         const day = id.substring(index)
         const selectedDay = new Date(monthDate.getFullYear(), monthDate.getMonth(), day)
-        // Проверка на то, мб кликнули на ту же дату
-        if (this.#startDay && selectedDay.valueOf() === this.#startDay.valueOf() ||
-            this.#finishDay && selectedDay.valueOf() === this.#finishDay.valueOf()) {
-            return false
+        // Проверка на то, мб кликнули на ту же дату (отмена выбора)
+        if (this.#startDay && selectedDay.valueOf() === this.#startDay.valueOf()) {
+            target.classList.remove('selected-day')
+            if (this.#finishDay) {
+                this.#startDay = this.#finishDay
+                this.#finishDay = null
+            } else {
+                this.#startDay = null
+            }
+            return true
+        }
+        if (this.#finishDay && selectedDay.valueOf() === this.#finishDay.valueOf()) {
+            target.classList.remove('selected-day')
+            this.#finishDay = null
+            return true
         }
         if (!this.#isResetBtn) {
             this.#resetBtn.classList.add('active-btn')
@@ -121,6 +140,78 @@ export class Calendar {
         } else if (this.#nextDate.getMonth() === date.getMonth() && this.#nextDate.getFullYear() === date.getFullYear()) {
             const day = document.getElementById(`right-${date.getDate()}`)
             day.classList.add('selected-day')
+        }
+        this.#renderBetweenDates(this.#currentDate, 'left-')
+        this.#renderBetweenDates(this.#nextDate, 'right-')
+    }
+
+    #renderBetweenDates(date, prefix) {
+        const daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+
+        if (!this.#startDay || !this.#finishDay) {
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                day.classList.remove('between-day')
+            }
+            return
+        }
+
+        const startBetweenDay = new Date(this.#startDay.getFullYear(),
+            this.#startDay.getMonth(), this.#startDay.getDate() + 1)
+        const finishBetweenDay = new Date(this.#finishDay.getFullYear(),
+            this.#finishDay.getMonth(), this.#finishDay.getDate() - 1)
+
+        const isStartDay = startBetweenDay.getMonth() === date.getMonth()
+            && startBetweenDay.getFullYear() === date.getFullYear()
+        const isFinishDay = finishBetweenDay.getMonth() === date.getMonth()
+            && finishBetweenDay.getFullYear() === date.getFullYear()
+        if (isStartDay && isFinishDay) {
+            const start = startBetweenDay.getDate()
+            const end = finishBetweenDay.getDate()
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                if (i >= start && i <= end) {
+                    day.classList.add('between-day')
+                } else {
+                    day.classList.remove('between-day')
+                }
+            }
+        } else if (isStartDay) {
+            const start = startBetweenDay.getDate()
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                if (i >= start) {
+                    day.classList.add('between-day')
+                } else {
+                    day.classList.remove('between-day')
+                }
+            }
+        } else if (isFinishDay) {
+            const end = finishBetweenDay.getDate()
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                if (i <= end) {
+                    day.classList.add('between-day')
+                } else {
+                    day.classList.remove('between-day')
+                }
+            }
+        } else if (startBetweenDay < date && finishBetweenDay > date) {
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                day.classList.add('between-day')
+            }
+        } else {
+            for (let i = 1; i <= daysInMonth; i++) {
+                const id = prefix + i
+                const day = document.getElementById(id)
+                day.classList.remove('between-day')
+            }
         }
     }
 
